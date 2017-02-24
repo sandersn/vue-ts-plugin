db5757f807803e86e8e98217173939588e7ebd26
Nathan Shively-Sanders <nathansa@microsoft.com>
2017-02-24 15:53:06 -0800

    Plugin inserts import and `new Vue` call
    Removed from parser. The parser does still have special-case code for
    setting up the scanner, but it's now triggered by a special parameter
    the plugin provides.

--- lib/index.ts
+++ lib/index.ts
@@ -3,6 +3,42 @@ import * as ts_module from "../node_modules/typescript/lib/tsserverlibrary";
 function init(modules: {typescript: typeof ts_module}) {
     const ts = modules.typescript;
 
+    /** Works like Array.prototype.find, returning `undefined` if no element satisfying the predicate is found. */
+    function find<T>(array: T[], predicate: (element: T, index: number) => boolean): T | undefined {
+        for (let i = 0; i < array.length; i++) {
+            const value = array[i];
+            if (predicate(value, i)) {
+                return value;
+            }
+        }
+        return undefined;
+    }
+
+    function modifyVueSource(sourceFile: ts.SourceFile): void {
+        // 1. add `import Vue from './vue'
+        // 2. find the export default and wrap it in `new Vue(...)` if it exists and is an object literal
+        const exportDefaultObject = find(sourceFile.statements, st => st.kind === ts.SyntaxKind.ExportAssignment &&
+                                         (st as ts.ExportAssignment).expression.kind === ts.SyntaxKind.ObjectLiteralExpression);
+        var b = <T extends ts.Node>(n: T) => ts.setTextRange(n, { pos: 0, end: 0 });
+        if (exportDefaultObject) {
+            const vueImport = b(ts.createImportDeclaration(undefined,
+                                                           undefined,
+                                                           b(ts.createImportClause(undefined,
+                                                                                   b(ts.createNamedImports([
+                                                                                       b(ts.createImportSpecifier(
+                                                                                           b(ts.createIdentifier("Vue")),
+                                                                                           b(ts.createIdentifier("Vue"))))])))),
+                                                           b(ts.createLiteral("./vue"))));
+            sourceFile.statements.unshift(vueImport);
+            const obj = (exportDefaultObject as ts.ExportAssignment).expression as ts.ObjectLiteralExpression;
+            (exportDefaultObject as ts.ExportAssignment).expression = ts.setTextRange(ts.createNew(ts.setTextRange(ts.createIdentifier("Vue"), { pos: obj.pos, end: obj.pos + 1 }),
+                                                                                                   undefined,
+                                                                                                   [obj]),
+                                                                                      obj);
+            ts.setTextRange(((exportDefaultObject as ts.ExportAssignment).expression as ts.NewExpression).arguments, obj);
+        }
+    }
+
     function create(info: ts.server.PluginCreateInfo) {
         // TODO: Maybe hook the document registry?
         // or add methods to the language service host, onCreateSourceFile/onUpdateSourceFile and add them as before/after advice.
@@ -31,11 +67,15 @@ function init(modules: {typescript: typeof ts_module}) {
             if (interested(filename)) {
                 // TODO: Cheat and expose the scanner instead, then replace the scanner.setText call
                 info.project.projectService.logger.info(`****** interested: ${filename} *****`);
-                const start = sourceText.indexOf("<script>") === -1 ? 0 : sourceText.indexOf("<script>") + "<script>".length;
-                const end = sourceText.indexOf("</script>");
-                sourceText = sourceText.slice(start, end);
+                //const start = sourceText.indexOf("<script>") === -1 ? 0 : sourceText.indexOf("<script>") + "<script>".length;
+                //const end = sourceText.indexOf("</script>");
+                //sourceText = sourceText.slice(start, end);
             }
-            return csf(filename, sourceText, languageVersion, setParentNodes, scriptKind);
+            var sourceFile = csf(filename, sourceText, languageVersion, setParentNodes, scriptKind, /*crazy*/ interested(filename));
+            if (interested(filename)) {
+                modifyVueSource(sourceFile);
+            }
+            return sourceFile;
         }
         ts.updateSourceFile = function (sourcefile: ts.SourceFile, newText: string, textChangeRange: ts.TextChangeRange, aggressiveChecks?: boolean) {
             // hmmm looks like this one isn't really used
